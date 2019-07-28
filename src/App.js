ae8b89f4fe663e0abcb58b4f4024e890bb89bd53
Bhavishya Negi <psspl.trainee32@outlook.com>
2019-07-28 23:53:58 +0530

    made a sign in form and did styling sign-in component custom-button component styling for custom-button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 // const HatsPage = () => (
 // 	<div>
@@ -13,9 +15,11 @@ import ShopPage from './pages/shop/shop';
 function App() {
 	return (
 		<div>
+			<Header/>
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route exact path="/shop" component={ShopPage} />
+				<Route exact path="/signin" component={SignInAndSignUpPage} />
 			</Switch>
 		</div>
 	);
